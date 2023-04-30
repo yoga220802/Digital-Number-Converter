2c8c3436589af6db09cead6f3ed4cd4a598f52f4
Yoga Agustiansyah <yogaagustiansyah01@gmail.com>
2023-04-30 22:33:23 +0700

    octal converter 30%

---
+++ src/components/functions/octal/toDecimal.ts
@@ -0,0 +1,8 @@
+import { ConverterResult } from "../../utils/customDataTypes"
+
+export const octalToDesimal = (octalNum: string) => {
+    let resultOtD: ConverterResult = {
+        converted: [],
+        explanation: []
+    }
+}
