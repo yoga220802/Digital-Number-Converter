0b64f630ea656806679ee713da833e91a0ec33ba
Yoga Agustiansyah <yogaagustiansyah01@gmail.com>
2023-08-05 21:46:40 +0700

    chore(file): rename file and add comments

---
+++ src/components/modules/index.ts
@@ -0,0 +1,138 @@
+// class Converter {
+//     constructor() {
+//         this.result = {
+//             converted: "",
+//             explanation: "",
+//         }
+//     }
+    
+//     decimal(number) {
+//         let decimal = DecimalConverter(number)
+//         this.result.converted = decimal.converted
+//         this.result.explanation = decimal.explanation
+
+//         return this.result
+//     }
+//     // 
+//     binary(number) {
+//         let binary = BinaryConverter(number)
+//         this.result.converted = binary.converted
+//         this.result.explanation = binary.explanation
+    
+//         return this.result
+//     }
+//     // 
+//     octal(number) {
+//         let octal = OctalConverter(number)
+//         this.result.converted = octal.converted
+//         this.result.explanation = octal.explanation
+    
+//         return this.result
+//     }
+//     // 
+//     hexadecimal(number) {
+//         let hexa = HexaConverter(number)
+//         this.result.converted = hexa.converted
+//         this.result.explanation = hexa.explanation
+        
+//         return this.result
+//     }
+//     // 
+// }
+
+// class DecimalConverter extends Converter {
+//     constructor (result, number) {
+//         super(result) 
+//         this.number = number
+//     }
+//     toBinary(number) {
+//         /*
+//         CODE TO BINARY
+//         ubah isi dari result
+//         */
+//     }
+//     toOctal(number) {
+//         /*
+//         CODE TO OCTAL
+//         ubah isi dari result
+//         */
+//     }
+//     toHexa(number) {
+//         /*
+//         CODE TO Hexa
+//         ubah isi dari result
+//         */
+//     }
+// }
+// class BinaryConverter extends Converter {
+//     constructor (result, number) {
+//         super(result)
+//         this.number = number
+//     }
+//     toDecimal(number) {
+//         /*
+//         CODE TO DECIMAL
+//         ubah isi dari result
+//         */
+//     }
+//     toOctal(number) {
+//         /*
+//         CODE TO OCTAL
+//         ubah isi dari result
+//         */
+//     }
+//     toHexa(number) {
+//         /*
+//         CODE TO Hexa
+//         ubah isi dari result
+//         */
+//     }
+// }
+// class OctalConverter extends Converter {
+//     constructor (result, number) {
+//         super(result)
+//         this.number = number
+//     }
+//     toBinary(number) {
+//         /*
+//         CODE TO BINARY
+//         ubah isi dari result
+//         */
+//     }
+//     toDecimal(number) {
+//         /*
+//         CODE TO DECIMAL
+//         ubah isi dari result
+//         */
+//     }
+//     toHexa(number) {
+//         /*
+//         CODE TO Hexa
+//         ubah isi dari result
+//         */
+//     }
+// }
+// class HexaConverter extends Converter {
+//     constructor (result, number) {
+//         super(result)
+//         this.number = number
+//     }
+//     toBinary(number) {
+//         /*
+//         CODE TO BINARY
+//         ubah isi dari result
+//         */
+//     }
+//     toDecimal(number) {
+//         /*
+//         CODE TO OCTAL
+//         ubah isi dari result
+//         */
+//     }
+//     toDecomal(number) {
+//         /*
+//         CODE TO DECIMAL
+//         ubah isi dari result
+//         */
+//     }
+// }
